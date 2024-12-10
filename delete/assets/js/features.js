b7ef8a1aa903a61f1272cf526d05b9def4f74d0c
ulcire <ericlugy@gmail.com>
2024-12-10 11:47:43 -0600

    Reformatting folder structure for proper pathing

---
+++ delete/assets/js/features.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  function adjustBorder() {
+    var pageW = document.body.offsetWidth;
+
+    var triangle = $(".triangle");
+    triangle.css("border-left", pageW + "px solid transparent");
+  }
+
+  $(window).on("resize", function () {
+    adjustBorder();
+  });
+
+  adjustBorder();
+});
